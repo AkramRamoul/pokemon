4aa5c62f5611a0c1516008895908ce5afe0c4785
AkramRamoul <ahmedakramramoul@gmail.com>
2024-11-22 15:23:08 +0100

    redirect buttin style fixes

---
+++ src/app/_components/RedirectButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useRouter } from "next/navigation";
+const RedirectButton = () => {
+  const router = useRouter();
+  return (
+    <button
+      onClick={() => router.push("/results")}
+      className="px-8 py-3 bg-blue-500 text-white rounded-lg text-lg font-semibold hover:bg-blue-600 transition-colors max-w-48"
+    >
+      View results
+    </button>
+  );
+};
+
+export default RedirectButton;
